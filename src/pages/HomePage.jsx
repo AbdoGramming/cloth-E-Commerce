c09029592af4159ddbb55eb5bcc0b63a32d313ed
AbdoGramming <114404614+kaet0@users.noreply.github.com>
2023-07-05 14:35:16 -1000

    * broken our homepage into smaller components * finished styling our menu-items * added some local fonts

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Directory from "../components/Directory";
+
+const HomePage = () => {
+  return (
+    <main className="homepage">
+      <Directory />
+    </main>
+  );
+};
+
+export default HomePage;
