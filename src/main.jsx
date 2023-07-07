dd77ce393c32b611469b6d69703dc018aa69f0c6
AbdoGramming <114404614+kaet0@users.noreply.github.com>
2023-07-06 19:32:45 -1000

    adding a a header component at the top of all layouts

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
 import "./sass/main.sass";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
+import Header from "./components/Header.jsx";
+import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage.jsx";
+import Layout from "./components/Layout";
 
-// const HatsPage = () => <h1>Hats Page</h1>;
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/shop",
-    element: <ShopPage />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />,
+      <Route path="shop" element={<ShopPage />} />,
+    </Route>,
+  ])
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
